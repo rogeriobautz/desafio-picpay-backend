92a4faf14298cbcc610f0afce350687c19e50222
Rogerio Bautz <rogeriobautz@gmail.com>
2024-04-02 23:40:26 -0300

    DTOs e Entity de Transaction criados

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,24 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty } from "class-validator";
+import { Column } from "typeorm";
+
+export class CreateTransactionDto {
+
+    constructor(valor: number, pagadorCpf: number, recebedorCpf: number){
+        this.valor = valor;
+        this.pagadorCpf = pagadorCpf;
+        this.recebedorCpf = recebedorCpf;
+    }
+
+
+    @Column({name: 'VALOR'})
+    @IsNotEmpty()
+    valor: number;
+    
+    @Column({name: 'PAGADOR'})
+    @IsNotEmpty()
+    pagadorCpf: number;
+    
+    @Column({name: 'RECEBEDOR'})
+    @IsNotEmpty()
+    recebedorCpf: number;
+}
--- src/transaction/entities/transaction.entity.ts
+++ src/transaction/entities/transaction.entity.ts
@@ -1,1 +1,28 @@
-export class Transaction {}
+import { IsNotEmpty } from "class-validator";
+import { Column, CreateDateColumn } from "typeorm";
+
+export class Transaction {
+
+    constructor(valor: number, pagadorCpf: number, recebedorCpf: number){
+        this.valor = valor;
+        this.pagadorCpf = pagadorCpf;
+        this.recebedorCpf = recebedorCpf;
+    }
+
+
+    @Column({name: 'VALOR'})
+    @IsNotEmpty()
+    valor: number;
+    
+    @Column({name: 'PAGADOR'})
+    @IsNotEmpty()
+    pagadorCpf: number;
+    
+    @Column({name: 'RECEBEDOR'})
+    @IsNotEmpty()
+    recebedorCpf: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+}
