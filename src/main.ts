f6c72e3b42d765f87443328637e234da620a859c
Rogerio Bautz <rogeriobautz@gmail.com>
2024-04-07 17:09:10 -0300

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,18 @@ async function bootstrap() {
     whitelist: true,
     transform: true
   }));
+
+ 
+  const config = new DocumentBuilder()
+    .setTitle('Desafio Picpay Backend (Junior)')
+    .setDescription('Desafio para vaga de dev backend junior feito em NestJS')
+    .setVersion('1.0')
+    .addTag('PicPay')
+    .build();
+  //await SwaggerModule.loadPluginMetadata(metadata);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
